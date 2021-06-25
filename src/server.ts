918d2d7d86afde787337d3a8b8ee129febe382d9
Max Antonio Raju <max.a.raju@gmail.com>
2021-06-25 14:03:21 +0100

    added router template

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { env } from "./environment/env";
 const port: number = env().port;
 import { App } from "./application";
 import { middleware } from "./middleware";
-import { routerTemplate } from "./routes/router.template";
+import { routerTemplate } from "./routes/template.router";
 const dbConString = env().db.uri(env().db.user, env().db.pw, env().db.name, env().db.account)
 
 /**
