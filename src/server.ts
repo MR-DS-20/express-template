4759f2ca0105535d61a8d2406580162338842ad7
Max Antonio Raju <max.a.raju@gmail.com>
2021-06-25 16:58:17 +0100

    reordered imports/declarations, formatted with prettier

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,37 @@
-// require("dotenv").config();
 import { env } from "./environment/env";
-const port: number = env().port ?? 8080;
 import { App } from "./application";
 import { middleware } from "./middleware";
 import { routerTemplate } from "./routes/template.router";
-let dbConString
+
+const port: number = env().port ?? 8080;
+let dbConString;
+
 try {
-    dbConString = env().db.uri(env().db.user, env().db.pw, env().db.name, env().db.account)
+  dbConString = env().db.uri(
+    env().db.user,
+    env().db.pw,
+    env().db.name,
+    env().db.account
+  );
 } catch {
-    console.log('Faild to create DB Connection string')
+  console.log("Failed to create DB Connection string");
 }
 
 /**
  * Configure App instance
  */
 const app = new App(
-    port,
-    middleware,
-    [routerTemplate] //* Add your express router objects here
+  port,
+  middleware,
+  [routerTemplate] //* Add your express router objects here
 );
 
 /**
  * Connect to MongoDB
  */
-dbConString ? app.mongoDB(dbConString) : console.log('Not Starting MongoDB Connection');
+dbConString
+  ? app.mongoDB(dbConString)
+  : console.log("Not Starting MongoDB Connection");
 
 /**
  * Launch!
