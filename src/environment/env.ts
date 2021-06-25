ba28933fad035778900169ae56ce6cf182fb09b5
Max Antonio Raju <max.a.raju@gmail.com>
2021-06-25 19:08:51 +0100

    formatted

--- src/environment/env.ts
+++ src/environment/env.ts
@@ -2,17 +2,17 @@ import { exit } from "process";
 import { IEnv } from "../interfaces/IEnv";
 export { IEnv } from "../interfaces/IEnv";
 
-
 export const env: () => IEnv = () => {
-    if (process.env.ENVIRONMENT === 'dev') {
-        let env = require('./dev')
-        return env
-    }
-    else if (process.env.ENVIRONMENT === 'production') {
-        let env = require('./prod')
-        return env
-    } else {
-        console.log(`Error. shell variable ENVIRONMENT not set. Acceptable values are 'dev' | 'production'`)
-        exit(1)
-    }
-}
+  if (process.env.ENVIRONMENT === "dev") {
+    let env = require("./dev");
+    return env;
+  } else if (process.env.ENVIRONMENT === "production") {
+    let env = require("./prod");
+    return env;
+  } else {
+    console.log(
+      `Error. shell variable ENVIRONMENT not set. Acceptable values are 'dev' | 'production'`
+    );
+    exit(1);
+  }
+};
