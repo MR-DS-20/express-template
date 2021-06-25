8ac86e1069a317c8c68991d14c5d78b63db6036a
Max Antonio Raju <max.a.raju@gmail.com>
2021-06-25 14:37:18 +0100

    comment added

--- src/controllers/controllers.module.ts
+++ src/controllers/controllers.module.ts
@@ -1,2 +1,6 @@
+/**
+ * Use this module file to create instances of all controllers and simplify imports in to your routers
+ */
+
 import { ExampleController } from './example.controller'
 export const exampleController = new ExampleController();
