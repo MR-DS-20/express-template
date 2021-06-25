b987c82f8268cf61908471981ec7106011d39cf8
Max Antonio Raju <max.a.raju@gmail.com>
2021-06-25 16:30:33 +0100

    refcatored to exit if no ENVIRONMENT set

--- src/environment/env.ts
+++ src/environment/env.ts
@@ -1,14 +1,18 @@
+import { exit } from "process";
 import { IEnv } from "../interfaces/IEnv";
 export { IEnv } from "../interfaces/IEnv";
 
 
-export const env:()=> IEnv = () =>{
-    if (process.env.ENVIRONMENT === 'dev'){
+export const env: () => IEnv = () => {
+    if (process.env.ENVIRONMENT === 'dev') {
         let env = require('./dev')
         return env
     }
-    if (process.env.ENVIRONMENT === 'production'){
-       let env = require('./prod')
-       return env
+    else if (process.env.ENVIRONMENT === 'production') {
+        let env = require('./prod')
+        return env
+    } else {
+        console.log(`Error. shell variable ENVIRONMENT not set. Acceptable values are 'dev' | 'production'`)
+        exit(1)
     }
 }
