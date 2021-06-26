6fec4701c87820234e1f6afc1108190b03fedcb2
Max Antonio Raju <max.a.raju@gmail.com>
2021-06-26 07:25:48 +0100

    adding another example function

--- src/controllers/example.controller.ts
+++ src/controllers/example.controller.ts
@@ -1,19 +1,21 @@
 import mongoose = require("mongoose");
-
 import { BaseController } from "./base.controller";
 import { Response, Request } from "express";
 import { ExampleModel } from "../models/example.model";
-
+import { ExampleDoc } from "../interfaces/example.interface";
 
 export class ExampleController extends BaseController {
   constructor() {
     super(new ExampleModel());
   }
 
-  exampleApiFunction(req: Request, res: Response) {
+  createFunction(req: Request, res: Response) {
     // Add some conditional logic...
-    this.create(res,req.body);
-}
+    this.create(res, req.body);
+  }
 
-  
+  async putFunction(req: Request, res: Response){
+    // const doc = await this.model.findById(req.params.id)
+    // doc
+  }
 }
