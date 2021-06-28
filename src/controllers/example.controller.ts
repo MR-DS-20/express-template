7c8381ee039f4ac506b603e0331c57bdda657cfc
Max Antonio Raju <max.a.raju@gmail.com>
2021-06-28 11:47:32 +0100

    developed put function example

--- src/controllers/example.controller.ts
+++ src/controllers/example.controller.ts
@@ -14,8 +14,15 @@ export class ExampleController extends BaseController {
     this.create(res, req.body);
   }
 
-  async putFunction(req: Request, res: Response){
-    // const doc = await this.model.findById(req.params.id)
-    // doc
+  async putFunction(req: Request, res: Response) {
+    try {
+      const doc = await this.model.findById<ExampleDoc>(req.params.id);
+      doc.title = req.body.title;
+      doc.set("details.name", req.body.details.name);
+      await doc.save();
+      this.jsonRes(doc, res);
+    } catch (e) {
+      this.errRes(e, res, "Failed");
+    }
   }
 }
