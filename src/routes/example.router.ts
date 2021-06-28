42ffc0376d2a13291bec88035d3a6a7804cc54bd
Max Antonio Raju <max.a.raju@gmail.com>
2021-06-28 11:48:36 +0100

    completed routes with examples

--- src/routes/example.router.ts
+++ src/routes/example.router.ts
@@ -6,11 +6,11 @@ export const routerTemplate = express.Router();
 import { exampleController } from "../controllers/controllers.module";
 
 // Set the common part of the path for the routes in this router
-const base = '/content'
+const base = '/example-path'
 
 //Routes
 routerTemplate.post(`${base}`, (req, res) => { exampleController.createFunction(req,res)})
-routerTemplate.put(`${base}/:id`, (req, res) => {  })
-routerTemplate.delete(`${base}/:id`, (req, res) => { })
-routerTemplate.get(`${base}/:id`, (req, res) => { })
+routerTemplate.put(`${base}/:id`, (req, res) => { exampleController.putFunction(req, res) })
+routerTemplate.delete(`${base}/:id`, (req, res) => { exampleController.deleteById(res, req.params.id)})
+routerTemplate.get(`${base}/:id`, (req, res) => { exampleController.findById(res, req.params.id)})
 
